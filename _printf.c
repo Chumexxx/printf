7684d7c34a74aae8d41222a1b61e96f4f58225a6
odamilolaakinyele <odamilolaakinyele@gmail.com>
2022-05-23 11:49:51 +0100

    task 0 update

--- _printf.c
+++ _printf.c
@@ -43,8 +43,7 @@ i += ev_print_func(format, i + 1);
 }
 else
 handl_buf(buffer, format[i], ibuf), len++;
-for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-;
+for (ibuf = len; ibuf > 1024; ibuf -= 1024);
 }
 print_buf(buffer, ibuf), free(buffer), va_end(arguments);
 return (len);
--- concatenate-buffer_chr.c
+++ concatenate-buffer_chr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
 * handl_buf - concatenates the buffer characters
 * @buf: buffer pointer
